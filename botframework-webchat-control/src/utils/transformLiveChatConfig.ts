f7e13e3e0ac6ac0a038c8f337888a322ad80ae5d
Edward Tran <edtran@microsoft.com>
2022-02-28 12:09:49 -0800

    Add configuration flag for persistent chat

--- botframework-webchat-control/src/utils/transformLiveChatConfig.ts
+++ botframework-webchat-control/src/utils/transformLiveChatConfig.ts
@@ -1,14 +1,17 @@
 export class ConfigurationManager {
     public static liveChatVersion: number = 1;
     public static canUploadAttachment: boolean = false;
+    public static isPersistentChat: boolean = false;
 }
 
 const transformLiveChatConfig = (liveChatConfig: any): ConfigurationManager => {
     const liveChatVersion = (liveChatConfig as any)["LiveChatVersion"];
     const canUploadAttachment = (liveChatConfig as any)["LiveWSAndLiveChatEngJoin"]["msdyn_enablefileattachmentsforcustomers"] === "true" || false;
+    const isPersistentChat = (liveChatConfig as any)["LiveWSAndLiveChatEngJoin"]["msdyn_conversationmode"] === "192350001" || false;
 
     ConfigurationManager.liveChatVersion = parseInt(liveChatVersion) || 1;
     ConfigurationManager.canUploadAttachment = canUploadAttachment;
+    ConfigurationManager.isPersistentChat = isPersistentChat;
 
     return ConfigurationManager;
 };
