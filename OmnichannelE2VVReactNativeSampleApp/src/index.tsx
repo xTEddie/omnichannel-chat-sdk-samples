3894b2c1925753254c1f877ab0f42238e448ba47
mumanasa <murtazamanasawala110@gmail.com>
2022-03-30 16:59:16 -0700

    Remove E2VV sample app from ChatSDK Samples

--- OmnichannelE2VVReactNativeSampleApp/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import 'node-libs-react-native/globals'
-import 'react-native-get-random-values';
-import { AppRegistry } from 'react-native';
-import App from './components/App';
-import { name as appName } from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
