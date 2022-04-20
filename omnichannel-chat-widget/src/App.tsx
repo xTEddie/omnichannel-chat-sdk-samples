4584b006d83e66933f2ee150668778ccefcea04d
Edward Tran <edtran@microsoft.com>
2022-04-20 14:27:05 -0700

    Install packages & Add live chat widget

--- omnichannel-chat-widget/src/App.tsx
+++ omnichannel-chat-widget/src/App.tsx
@@ -1,12 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { OmnichannelChatSDK } from "@microsoft/omnichannel-chat-sdk";
+import {LiveChatWidget} from "@microsoft/omnichannel-chat-widget";
 import './App.css';
 
+const fetchOmnichannelChatConfig = () => {
+  const omnichannelConfig = { // Default config
+    orgId: process.env.REACT_APP_orgId || '',
+    orgUrl: process.env.REACT_APP_orgUrl || '',
+    widgetId: process.env.REACT_APP_widgetId || ''
+  };
+
+  return omnichannelConfig;
+}
+
 function App() {
+  const [liveChatWidgetProps, setLiveChatWidgetProps] = useState<any>();
+
+  useEffect(() => {
+    const init = async () => {
+      const omnichannelConfig = fetchOmnichannelChatConfig();
+
+      const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+      await chatSDK.initialize();
+      const chatConfig = await chatSDK.getLiveChatConfig();
+
+      const liveChatWidgetProps = {
+        chatSDK,
+        chatConfig
+      };
+
+      setLiveChatWidgetProps(liveChatWidgetProps);
+    }
+
+    init();
+  }, []);
+
   return (
     <div className="App">
       <div>
-
+        <LiveChatWidget {...liveChatWidgetProps} />
       </div>
     </div>
   );
