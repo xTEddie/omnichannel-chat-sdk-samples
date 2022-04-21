16b86982a269de7b24821e7c152a0cbfab1aac27
Edward Tran <edtran@microsoft.com>
2022-04-20 17:33:55 -0700

    Add ability to pass omnichannel config via url params

--- omnichannel-chat-widget/src/App.tsx
+++ omnichannel-chat-widget/src/App.tsx
@@ -10,6 +10,21 @@ const fetchOmnichannelChatConfig = () => {
     widgetId: process.env.REACT_APP_widgetId || ''
   };
 
+  const urlParams = new URLSearchParams(window.location.search);
+
+  // Overrides default config if URL any param is found
+  if (urlParams.get('orgUrl') !== null) {
+    omnichannelConfig.orgUrl = urlParams.get('orgUrl')!;
+  }
+
+  if (urlParams.get('orgId') !== null) {
+    omnichannelConfig.orgId = urlParams.get('orgId')!;
+  }
+
+  if (urlParams.get('widgetId') !== null) {
+    omnichannelConfig.widgetId = urlParams.get('widgetId')!;
+  }
+
   return omnichannelConfig;
 }
 
