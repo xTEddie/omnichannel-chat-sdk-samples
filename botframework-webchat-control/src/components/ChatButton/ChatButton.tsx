a3c3747774496c102f935403531c48cf0a5ba6e3
Edward Tran <edtran@microsoft.com>
2022-09-23 14:52:18 -0700

    Add props to render button to the left

--- botframework-webchat-control/src/components/ChatButton/ChatButton.tsx
+++ botframework-webchat-control/src/components/ChatButton/ChatButton.tsx
@@ -3,12 +3,13 @@ import { MessageCircle } from "react-feather";
 import './ChatButton.css';
 
 interface ChatButtonProps {
-  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void,
+  left?: Boolean
 }
 
 function ChatButton(props: ChatButtonProps) {
   return (
-    <div className="chat-button" onClick={props.onClick}>
+    <div className={`chat-button ${props.left? 'left': ''}`} onClick={props.onClick}>
       <MessageCircle color='white' />
     </div>
   );
