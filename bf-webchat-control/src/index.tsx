0e6d5648ca0b80fafffb9ad1f1ba0c8875c4df66
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-01-16 18:53:04 -0800

    Configure project

--- bf-webchat-control/src/index.tsx
+++ bf-webchat-control/src/index.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import WebChat from './components/WebChat/WebChat';
 import reportWebVitals from './reportWebVitals';
+import { StateProvider } from './context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StateProvider>
+      <WebChat />
+    </StateProvider>    
   </React.StrictMode>
 );
 
